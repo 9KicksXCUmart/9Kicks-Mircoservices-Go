97d480d3938d6be1c1fed0ea308fa829a71fad6b
nickwkt <nickb55245@gmail.com>
2024-04-07 20:05:14 +0800

    chore: change update stock method to POST

--- router/product.go
+++ router/product.go
@@ -13,5 +13,5 @@ func ProductRegister(r *gin.Engine) {
 	r.GET("/v1/products/:id/stock", controller.GetStock)
 	r.PUT("/v1/products/update-detail", controller.UpdateProductInfo)
 	r.DELETE("/v1/products/:id", controller.DeleteProduct)
-	r.PATCH("/v1/products/:id", controller.UpdateStock)
+	r.POST("/v1/products/:id", controller.UpdateStock)
 }
