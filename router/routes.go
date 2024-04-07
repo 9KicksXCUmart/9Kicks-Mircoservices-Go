72a422e959e938646affb2772c285d9ebcfed253
nickwkt <nickb55245@gmail.com>
2024-04-08 03:29:27 +0800

    chore: allow all origins

--- router/routes.go
+++ router/routes.go
@@ -9,17 +9,19 @@ func SetupRouter() *gin.Engine {
 	route := gin.Default()
 
 	// CORS
-	config := cors.DefaultConfig()
-	config.AllowOrigins = []string{"http://localhost:5173", "https://9kicks.shop"}
-	config.AllowMethods = []string{"GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"}
-	config.AllowHeaders = []string{"Origin", "Content-Length", "Content-Type", "Authorization", "Access-Control-Allow-Origin", "Access-Control-Allow-Headers", "Access-Control-Allow-Methods"}
-	config.AllowCredentials = true
-	route.Use(cors.New(config))
+	//config := cors.DefaultConfig()
+	//config.AllowOrigins = []string{"http://localhost:5173", "https://9kicks.shop"}
+	//config.AllowMethods = []string{"GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"}
+	//config.AllowHeaders = []string{"Origin", "Content-Length", "Content-Type", "Authorization", "Access-Control-Allow-Origin", "Access-Control-Allow-Headers", "Access-Control-Allow-Methods"}
+	//config.AllowCredentials = true
+	//route.Use(cors.New(config))
 
 	// register all routes.
 	AuthRegister(route)
 	ReviewRegister(route)
 	ProductRegister(route)
+
+	route.Use(cors.Default())
 
 	return route
 }
